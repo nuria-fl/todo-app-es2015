88fb8f7def89741eb88d17216765cb8d1383d283
Nuria <soriano.nuria@gmail.com>
2016-08-10 10:22:31 +0200

    make it work

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,72 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
 
-export default ({ config, db }) => {
+export default () => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	var tasks = [];
+	var counter = 0;
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
+	api.get('/', (req, res)=>{
+		var toDoTasks = tasks.filter(function(elem){
+			return elem.completed === false;
+		});
+		res.render('index', {
+			title: 'Todo',
+			tasks: toDoTasks,
+			currentUrl: req.url
+		});
+	})
+
+	api.post('/', (req, res)=>{
+		var newTask = {
+			id: ++counter,
+			name: req.body.name,
+			createdDate: new Date(),
+			completed: false
+		};
+		tasks.push(newTask);
+		
+		res.redirect('/');
+	});
+	api.put('/', (req, res)=>{
+		var taskId = parseInt(req.query.id);
+		tasks.forEach(function(elem){
+			if(elem.id === taskId){
+				elem.completed = true;
+				elem.completedDate = new Date();
+			}
+		});
+		res.end();
+	});
+	api.delete('/', (req, res)=>{
+		var taskId = parseInt(req.query.id);
+		var itemToDelete = null;
+		tasks.forEach(function(elem, i){
+			if(elem.id === taskId){
+				itemToDelete = i;
+			}
+		});
+		tasks.splice(itemToDelete, 1);
+		res.end();
+	});
+	api.put('/alldone', (req, res)=>{
+		tasks.forEach(function(elem){
+			elem.completed = true;
+			elem.completedDate = new Date();
+		});
+		res.end();
+	});
+
+	api.get('/completed', (req, res)=>{
+		var doneTasks = tasks.filter(function(elem){
+			return elem.completed === true;
+		});
+		res.render('completed', {
+			title: 'Todo',
+			tasks: doneTasks,
+			currentUrl: req.url
+		});
 	});
 
 	return api;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
 import http from 'http';
 import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
-import initializeDb from './db';
-import middleware from './middleware';
+import jade from 'jade';
+// import middleware from './middleware';
 import api from './api';
 import config from './config.json';
 
 let app = express();
-app.server = http.createServer(app);
 
-// 3rd party middleware
-app.use(cors({
-	exposedHeaders: config.corsHeaders
-}));
+app.locals.moment = require('moment');
 
-app.use(bodyParser.json({
-	limit : config.bodyLimit
-}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
-// connect to db
-initializeDb( db => {
+// internal middleware
+// app.use(middleware({ config, db }));
 
-	// internal middleware
-	app.use(middleware({ config, db }));
+// api router
+app.use('/', api());
 
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(process.env.PORT || config.port);
-
-	console.log(`Started on port ${app.server.address().port}`);
-});
+app.listen(process.env.PORT || '3000');
 
 export default app;
