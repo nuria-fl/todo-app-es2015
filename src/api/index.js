2acddbb5dfd0dcb410f7892f5bb224c242dcd0be
Nuria <soriano.nuria@gmail.com>
2016-08-10 10:36:28 +0200

    separate functions to a different file

--- src/api/index.js
+++ src/api/index.js
@@ -1,74 +1,16 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
+import funcs from '../middleware/index';
 
 export default () => {
 	let api = Router();
-
-	var tasks = [];
-	var counter = 0;
-
-	api.get('/', (req, res)=>{
-		var toDoTasks = tasks.filter(function(elem){
-			return elem.completed === false;
-		});
-		res.render('index', {
-			title: 'Todo',
-			tasks: toDoTasks,
-			currentUrl: req.url
-		});
-	})
-
-	api.post('/', (req, res)=>{
-		var newTask = {
-			id: ++counter,
-			name: req.body.name,
-			createdDate: new Date(),
-			completed: false
-		};
-		tasks.push(newTask);
-		
-		res.redirect('/');
-	});
-	api.put('/', (req, res)=>{
-		var taskId = parseInt(req.query.id);
-		tasks.forEach(function(elem){
-			if(elem.id === taskId){
-				elem.completed = true;
-				elem.completedDate = new Date();
-			}
-		});
-		res.end();
-	});
-	api.delete('/', (req, res)=>{
-		var taskId = parseInt(req.query.id);
-		var itemToDelete = null;
-		tasks.forEach(function(elem, i){
-			if(elem.id === taskId){
-				itemToDelete = i;
-			}
-		});
-		tasks.splice(itemToDelete, 1);
-		res.end();
-	});
-	api.put('/alldone', (req, res)=>{
-		tasks.forEach(function(elem){
-			elem.completed = true;
-			elem.completedDate = new Date();
-		});
-		res.end();
-	});
-
-	api.get('/completed', (req, res)=>{
-		var doneTasks = tasks.filter(function(elem){
-			return elem.completed === true;
-		});
-		res.render('completed', {
-			title: 'Todo',
-			tasks: doneTasks,
-			currentUrl: req.url
-		});
-	});
+	
+	api.get('/', funcs.listTasks);
+	api.post('/', funcs.newTask );
+	api.put('/', funcs.completeTask);
+	api.delete('/', funcs.deleteTask);
+	api.put('/alldone', funcs.completeAllTasks);
+	api.get('/completed', funcs.listCompletedTasks);
 
 	return api;
 }
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,71 @@
-import { Router } from 'express';
+let funcs = {};
+var tasks = [];
+var counter = 0;
 
-export default ({ config, db }) => {
-	let routes = Router();
-
-	// add middleware here
-
-	return routes;
+// add middleware here
+funcs.listTasks = (req, res)=>{
+	var toDoTasks = tasks.filter(function(elem){
+		return elem.completed === false;
+	});
+	res.render('index', {
+		title: 'Todo',
+		tasks: toDoTasks,
+		currentUrl: req.url
+	});
 }
+
+funcs.newTask = (req, res)=>{
+	var newTask = {
+		id: ++counter,
+		name: req.body.name,
+		createdDate: new Date(),
+		completed: false
+	};
+	tasks.push(newTask);
+	
+	res.redirect('/');
+}
+
+funcs.deleteTask = (req, res)=>{
+	var taskId = parseInt(req.query.id);
+	var itemToDelete = null;
+	tasks.forEach(function(elem, i){
+		if(elem.id === taskId){
+			itemToDelete = i;
+		}
+	});
+	tasks.splice(itemToDelete, 1);
+	res.end();
+}
+
+funcs.completeTask = (req, res)=>{
+	var taskId = parseInt(req.query.id);
+	tasks.forEach(function(elem){
+		if(elem.id === taskId){
+			elem.completed = true;
+			elem.completedDate = new Date();
+		}
+	});
+	res.end();
+}
+
+funcs.completeAllTasks = (req, res)=>{
+	tasks.forEach(function(elem){
+		elem.completed = true;
+		elem.completedDate = new Date();
+	});
+	res.end();
+}
+
+funcs.listCompletedTasks = (req, res)=>{
+	var doneTasks = tasks.filter(function(elem){
+		return elem.completed === true;
+	});
+	res.render('completed', {
+		title: 'Todo',
+		tasks: doneTasks,
+		currentUrl: req.url
+	});
+}
+
+export default funcs;
